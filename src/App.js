3c3cdf9f8ec3053c107aa48590ed6e6ca32e682b
lizardkingLK <chansanfdo@gmail.com>
2022-10-08 18:02:35 +0530

    added app.js to do the routing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/home";
+
+const App = () => {
+    return (
+        <div className="container">
+            <Home />
+        </div>
+    )
+};
+
+export default App;
